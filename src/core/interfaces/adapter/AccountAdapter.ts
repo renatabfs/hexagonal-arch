94c069bde3612ca86839ef5e88fda8ee3a393b8a
renatabfs <renata.niall66@gmail.com>
2022-11-10 09:01:03 -0300

    updated account adapter with account service interface

--- src/core/interfaces/adapter/AccountAdapter.ts
+++ src/core/interfaces/adapter/AccountAdapter.ts
@@ -2,4 +2,8 @@ import Account from '../../domain/models/Account';
 
 abstract class AccountAdapter {
     abstract fetch(): Promise<Account[]>
+
+    abstract create(account: Account): Promise<boolean>;
+    abstract update(account: Account): Promise<boolean>;
+    abstract delete(accountId: number): Promise<boolean>;
 } export default AccountAdapter;
