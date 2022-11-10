6501d0a00db1ae195d30dd0b3d74524f96769fcd
renatabfs <renata.niall66@gmail.com>
2022-11-10 11:55:13 -0300

    updated with container

--- index.ts
+++ index.ts
@@ -1,14 +1,13 @@
-import FoodService from "./src/core/services/FoodService";
-import FoodAPI from "./src/infra/api/food";
 
-const service = new FoodService(new FoodAPI());
+import APIDIContainer from "./src/core/dicontainer/api";
+import WebDIContainer from "./src/core/dicontainer/web/index";
 
-service.fetch();
+const service = new WebDIContainer().getAccountService();
 
-async function getFoods() {
-    const foods = await service.fetch();
-    for (const food of foods){
-        console.log(food.name);
-    }
+async function  getFoods() {
+const foods = await service.fetch();
+
+for (const food of foods) {
+    console.log(food.name);
 }
-getFoods()
+} getFoods()
