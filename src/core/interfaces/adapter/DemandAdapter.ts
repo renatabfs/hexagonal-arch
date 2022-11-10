707e259c35562ca33fc1b11c205a38c150c0b535
renatabfs <renata.niall66@gmail.com>
2022-11-10 09:01:13 -0300

    updated demand adapter with demand service interface

--- src/core/interfaces/adapter/DemandAdapter.ts
+++ src/core/interfaces/adapter/DemandAdapter.ts
@@ -3,5 +3,7 @@ import Demand from '../../domain/models/Demand'
 abstract class DemandAdapter {
     abstract fetch(): Promise<Demand[]>;
     abstract fetchByAccount(accountId: number): Promise<Demand[]>
-
+    abstract update(demand: Demand): Promise<boolean>;
+    abstract delete(demandId: number): Promise<boolean>;
+    abstract create(demand: Demand): Promise<boolean>;
 } export default DemandAdapter;
