d786cbb092eee91efd6417718c0b9b30086ef505
renatabfs <renata.niall66@gmail.com>
2022-11-09 21:32:08 -0300

    added food infrastructure

---
+++ src/core/domain/models/infra/api/food/index.ts
@@ -0,0 +1,18 @@
+import FoodAdapter from "../../../../../interfaces/adapter/FoodAdapter";
+import { DTO } from "../../../../types/DTO";
+import Food from "../../../Food";
+import { APIClient } from "../clients/APIClients";
+
+class FoodAPI extends FoodAdapter {
+    async fetch(): Promise<Food[]> {
+    const response = await APIClient.get<DTO[]>('/foods')
+    return response.data.map(Food.fromJSON)
+    }
+
+    async create(food: Food): Promise<boolean>{
+        await APIClient.post("/foods", food.toJSON());
+        return true;
+    }
+}
+
+export default FoodAPI;
